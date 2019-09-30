a8867dde5b4681e9115e46dff39cea4518c9f7bf
mandaputtra <mandaputra8@gmail.com>
2019-09-30 13:36:45 +0800

    adding web performance example

--- web_performance_demo/src/intersection-observer.js
+++ web_performance_demo/src/intersection-observer.js
@@ -29,26 +29,4 @@ const intersectionObserver = new IntersectionObserver(entries => {
 intersectionObserver.observe(sentinel);
 
 // Intersection observer to load images if it almost show up
-// code are same as above
-// http://placehold.jp/300x150.png
-const imgContainer = document.querySelector('#image_container')
-const space = document.querySelector('#no-image')
-
-function loadImage(n) {
-  for (let i = 0; i < n; i++) {
-    let img = document.createElement('img')
-    img.src = 'http://placehold.jp/300x150.png'
-    imgContainer.appendChild(img)
-  }
-}
-
-const imageObserver = new IntersectionObserver(entries => {
-  if (entries[0].intersectionRatio <= 0) {
-    return;
-  }
-  loadItems(10);
-  // appendChild will move the existing element
-  scroller.appendChild(sentinel);
-  loadItems(5);
-  cc.innerHTML = `Already Load ${counter}`
-})
+// https://codepen.io/rpsthecoder/pen/pByZjR?editors=1010
